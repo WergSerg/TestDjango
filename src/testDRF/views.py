0b18714e481a39a6f84c1e86bcae20599b9fce16
WergSerg <wwwser181@outlook.com>
2021-11-15 16:10:28 +0300

    Добавление фукнционала: добавлена Пагинация

--- src/testDRF/views.py
+++ src/testDRF/views.py
@@ -1,14 +1,14 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.views import csrf_exempt
-from rest_framework import  viewsets
+from rest_framework import  viewsets,pagination
+from django.conf import settings
 
 from .models import Depart,Employeers
 from .serializers import EmployeerSerializer,DepartSerializer
 
-
 class EmployeerModeViewSet(viewsets.ModelViewSet):
     queryset = Employeers.objects.all()
+    pagination_class = pagination.LimitOffsetPagination
     serializer_class = EmployeerSerializer
     def get_queryset(self):
         queryset = Employeers.objects.all()
@@ -32,7 +32,7 @@ class StatListView(ListAPIView):
 
     serializer_class = DepartSerializer
 
-    @csrf_exempt
+
     def list(self, request):
         queryset = self.get_queryset()
         serializer = DepartSerializer(list(queryset), many=True)
