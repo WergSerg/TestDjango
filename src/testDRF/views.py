ef7a954aa2d9d036a0b85728af8059b11fc8e0eb
WergSerg <wwwser181@outlook.com>
2021-11-17 15:23:06 +0300

    добавил разграниечение прав

--- src/testDRF/views.py
+++ src/testDRF/views.py
@@ -1,7 +1,8 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework import  viewsets,pagination
-from django.conf import settings
+from rest_framework.permissions import AllowAny
+
 
 from .models import Depart,Employeers
 from .serializers import EmployeerSerializer,DepartSerializer
@@ -24,12 +25,14 @@ class EmployeerModeViewSet(viewsets.ModelViewSet):
 
 
 class StatListView(ListAPIView):
+    permission_classes  = [AllowAny]
     queryset = Depart.objects.raw('SELECT a1."id","depName","depDirect" , count(a2."fullName"),sum(a2."salary") '\
             'FROM "testDRF_depart" as a1 '\
             'left join "testDRF_employeers" as a2 '\
             'on a1."id"=a2."id" '\
             'group by a1."id"')
     serializer_class = DepartSerializer
+
 
     def list(self, request):
         queryset = self.get_queryset()
